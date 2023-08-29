c7e3a25f02625792ee52644dfe39a8a17e516cdc
Adrian Schneider <adrian.schneider.pr@gmail.com>
2023-08-29 11:02:22 +0200

    Clean code

--- simple.py
+++ simple.py
@@ -1,4 +1,4 @@
-# resources: https://github.com/xelest/tapo_c200_python_opencv
+# Resources: https://github.com/xelest/tapo_c200_python_opencv
 
 import argparse
 from datetime import datetime
@@ -7,25 +7,21 @@ import cv2    # pip install opencv-python
 
 import img_proc as ip
 
-parser = argparse.ArgumentParser(description='Record images when motion')
-parser.add_argument('user')
-parser.add_argument('password')
-parser.add_argument('ip')
-
+parser = argparse.ArgumentParser(description='Record images from tapo camera when there is motion.')
+parser.add_argument('user', help='Username of the tapo camera')
+parser.add_argument('password', help='Password of the tapo camera')
+parser.add_argument('ip', help='IP address of the tapo camera')
+parser.add_argument('threshold', help='MSE threshold for recording a video sequence', type=float)
 args = parser.parse_args()
 
-user = args.user # user you set in Advanced Settings -> Camera Account
-password = args.password # password you set in Advanced Settings -> Camera Account
-host = args.ip # ip of the camera, example: 192.168.1.52
 port = 554
 length_recording = 80 # frames
 length_prev_recording_buffer = 20 # frames
 
+url_lowres = f"rtsp://{args.user}:{args.password}@{args.ip}:{port}/stream2" # stream1 -> higher resolution
+print(url_lowres)
 
-url_1080p = f"rtsp://{user}:{password}@{host}:{port}/stream2" # stream1 -> higher resolution
-print(url_1080p)
-
-cap = cv2.VideoCapture(url_1080p)
+cap = cv2.VideoCapture(url_lowres)
 
 if not cap.isOpened():
     print("Failed to open RTSP stream")
@@ -41,20 +37,21 @@ frame_ring_buffer = collections.deque(maxlen=length_prev_recording_buffer)
 while True:
     # Read a frame from the RTSP stream and convert to grayscale
     ret, frame = cap.read()
+    if not ret:
+        print("Failed to read frame from camera")
+        break
     h, w, c = frame.shape
     frame_gs = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) if c == 3 else frame
 
+    # Keep recorded frames in a ringbuffer
     frame_ring_buffer.append(frame_gs)
-
-    # Check if the frame is read correctly
-    if not ret:
-        print("Failed to read frame")
-        break
 
     if is_recording:
         k_recording += 1
         cv2.imshow("RTSP Stream", frame_gs)
         video_writer.write(frame_gs)
+
+        # Stop recording when reaching defined number of frames
         if k_recording > length_recording:
             is_recording = False
             prev_frame = None
@@ -67,7 +64,7 @@ while True:
             mse = ip.mean_square_error(frame_gs, prev_frame)
 
             # record a movie if there is motion
-            if mse > 80.0:
+            if mse > args.threshold:
                 is_recording = True
                 k_recording = 0
 
@@ -75,6 +72,8 @@ while True:
                 recording_file_name = f"recs/{datetime.now():%Y-%m-%d_%H-%M-%S}-MSE={mse}.avi"
                 video_writer = cv2.VideoWriter(recording_file_name, cv2.VideoWriter_fourcc(*'MJPG'), 10, (w, h),
                                                isColor=False)
+
+                # write whole frame buffer to the newly created video file
                 while frame_ring_buffer:
                     video_writer.write(frame_ring_buffer.popleft())
 
